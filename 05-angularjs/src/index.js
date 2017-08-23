83d17b8fb94690bdce21b8dc1e26f1e709742a96
Paula Aymerich <p.aymerich.f@gmail.com>
2017-08-23 12:10:26 +0200

    TP05-02

--- 05-angularjs/src/index.js
+++ 05-angularjs/src/index.js
@@ -8,3 +8,8 @@ console.log('Bonjour App');
 import tplTp01 from './tp01/expressions.html'
 // insertion du code HTML dans le corps de la page principale
 document.querySelector('body').innerHTML = [tplTp01].join('<hr>')
+
+// récupération de la page HTML ./tp01/expressions.html à l'aide de Webpack
+import tplTp02 from './tp02/vues.html'
+// insertion du code HTML dans le corps de la page principale
+document.querySelector('body').innerHTML = [tplTp02].join('<hr>')
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ JS practics:
 - TP03: Webpack
 - TP04: ES2015, PROMISE
 - TP05: Angular JS
+    - tp01: expressions
+    - tp02: Vues et Expressions
